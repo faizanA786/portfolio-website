26ea88e633c43e9697ad3d8d45aa2b3a67f89b11
faizanA786 <faizan65650@gmail.com>
2026-01-21 15:30:18 +0000

    chore: Update projects and CV

--- docs/js/projects.js
+++ docs/js/projects.js
@@ -6,9 +6,12 @@ function openInNewWindow(event) {
     else if(imageId == 2) {
         window.open("images/demo/blackjack.gif", "_blank");
     }
+    else if(imageId == 3) {
+        window.open("images/demo/workportal.gif", "_blank");
+    }
 }
 
-let gifArray = [document.getElementById("1"), document.getElementById("2")];
+let gifArray = [document.getElementById("1"), document.getElementById("2"), document.getElementById("3")];
 
 for (let i=0; i<gifArray.length; i++) {
     gifArray[i].addEventListener("click", openInNewWindow);
